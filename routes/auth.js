1843e1c26277e7c4af5faac0e687635e089bc2a1
Tyler Dance <tylerdance@Tylers-iMac.fios-router.home>
2020-11-23 16:04:39 -0500

    [auth] add sign up POST route

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const passport = require('passport');
+const db = require('../models');
 const router = express.Router();
+// const db = require('../models');
+// const passport = require('../config/ppConfig')
 
 router.get('/signup', (req, res) => {
   res.render('auth/signup');
@@ -8,5 +12,40 @@ router.get('/signup', (req, res) => {
 router.get('/login', (req, res) => {
   res.render('auth/login');
 });
+
+router.post('/signup', (req, res) => {
+  console.log(req.body);
+
+
+// make sure user doesn't already exist
+  db.user.findOrCreate({
+    where: { email: req.body.email },
+    defaults: {
+      name: req.body.name,
+      password: req.body.password
+    }
+  })
+  .then(([user, created]) => {
+    if (created) {
+      // if created, success and redirect back to home
+      console.log(`${user.name} was created`);
+      // flash message
+      const successObject = {
+        successRedirect: '/',
+        successFlash: 'Account created and logging in...'
+      }
+      passport.authenticate('local', successObject) (req, res);
+    } else {
+      // email already exists
+      req.flash('error', 'Email already exists')
+      res.redirect('/auth/signup');
+    }
+  })
+  .catch(err => {
+    console.log('Error', err);
+    req.flash('error', 'Either email or password is incorrect. Please try again.');
+    res.redirect('/auth/signup');
+  })
+})
 
 module.exports = router;
