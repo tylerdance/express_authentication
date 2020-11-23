4fa9166c02f6945429813830791715a8df00b8c9
Tyler Dance <tylerdance@Tylers-iMac.fios-router.home>
2020-11-23 12:50:46 -0500

    [server] add sessionObject for express-sessions

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const layouts = require('express-ejs-layouts');
+const session = require('express-session');
+const passport = require('./config/ppConfig');
+const flash = require('connect-flash')
+const SECRET_SESSION = process.env.SECRET_SESSION
+console.log(SECRET_SESSION);
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -9,6 +14,17 @@ app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(layouts);
+
+// secret: what user will be given on site as a session cookie
+// resave: saves session even if it's modifie, make this false
+// saveUninitialized: if new session, it is saved, making it true
+const sessionObject = {
+  secret: SECRET_SESSION,
+  resave: false,
+  saveUninitialized: true
+}
+
+app.use(session(sessionObject));
 
 app.get('/', (req, res) => {
   res.render('index');
